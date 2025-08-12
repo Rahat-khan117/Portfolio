85d4fffbd449b24593eefcafac5d82dbb4c6e62f
Rahat-khan117 <rahatkhan015002@gmail.com>
2025-08-12 17:10:26 +0600

    skills added

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -7,22 +7,104 @@ const Skills = () => {
       <p className="text-5xl font-bold text-center">My skills</p>
 
 
-      <div className="grid  md:grid-cols-2">
+      <div className="grid  md:grid-cols-2 gap-8">
         <div className="mt-9">
             <p className="text-2xl text-center font-bold">Frontend skills</p>
-            <div className="grid grid-cols-3 mt-12">
+            <div className="grid grid-cols-3 gap-5 md:gap-4 lg:gap-7  mt-12 w-fit mx-auto place-items-center">
                <div className="flex flex-col gap-2 items-center">
-                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-600 flex justify-center items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
                    <img className="h-[65px] w-[65px]" src="../images/html.png" />
                   </div>
                   <p className="text-xl mt-2 font-bold">HTML5</p>
+                  <p className="-mt-2">Intermediate</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/css-3.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">CSS-3</p>
+                  <p className="-mt-2">Expert</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/tailwind-css-logo.svg" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">Tailwind</p>
+                  <p className="-mt-2">Intermediate</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/react.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">React Js</p>
+                  <p className="-mt-2">Intermediate</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[60px] w-[60px]" src="../images/js.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">JavaScript</p>
+                  <p className="-mt-2">Intermediate</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/figma.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">Figma</p>
                   <p className="-mt-2">Intermediate</p>
                </div>
                
             </div>
         </div>
 
-        <div></div>
+        <div className="mt-9">
+            <p className="text-2xl text-center font-bold">Backend skills</p>
+            <div className="grid grid-cols-3 gap-5 md:gap-4 lg:gap-7 mt-12 w-fit mx-auto place-items-center">
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/mongodb.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">MongoDB</p>
+                  <p className="-mt-2">Intermediate</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/express.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">Express</p>
+                  <p className="-mt-2">Intermediate</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/nextjs.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">Next Js</p>
+                  <p className="-mt-2">Beginner</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/Redux.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">Redux</p>
+                  <p className="-mt-2">beginner</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[60px] w-[60px]" src="../images/node-js.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">Node js</p>
+                  <p className="-mt-2">Beginner</p>
+               </div>
+               <div className="flex flex-col gap-2 items-center">
+                  <div className="h-[110px] w-[90px] rounded-[46%_54%_56%_44%_/38%_34%_66%_62%] bg-gray-700 flex justify-center items-center">
+                   <img className="h-[65px] w-[65px]" src="../images/typescript.png" />
+                  </div>
+                  <p className="text-xl mt-2 font-bold">TypeScript</p>
+                  <p className="-mt-2">Intermediate</p>
+               </div>
+               
+            </div>
+        </div>
       </div>
     </div>
   );
